7afbebd5c0ecd128a21173ba56646b5da8569dc5
Mikhail <mikhail@MacBook-Pro-Mikhail.local>
2023-02-02 21:59:30 +0300

    ci

--- json_test.py
+++ json_test.py
@@ -4,10 +4,11 @@ import io
 
 with io.open("test.json", 'r') as f:
         settings = json.load(f)
-        for setting in settings:
-                alert_name = setting.get["alert_name"]
-                regis_id = setting.get["regis_id"]
-                ignore = setting.get["ignore"]
+        alerts_list = settings["alerts"]
+        for alert in alerts_list:
+                alert_name = alert.get["alert_name"]
+                regis_id = alert.get["regis_id"]
+                ignore = alert.get["ignore"]
                 if alert_name == None or regis_id == None or ignore == None:
                         raise Exception("Can not pasrse json")
                                 
