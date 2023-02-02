1f61af86e7158cd7c1d72e7faceb9680e7b3a72b
Mikhail <mikhail@192.168.1.11>
2023-02-02 22:41:45 +0300

    ci

--- json_test.py
+++ json_test.py
@@ -7,15 +7,15 @@ if __name__ == "__main__":
                 settings = json.load(f)
                 alerts_list = settings.get("alerts")
                 if alerts_list == None:
-                        raise Exception(f"Can not parse json. No alerts list")
+                        raise Exception("Can not parse json. No alerts list")
                 for alert_settings in alerts_list:
                         alert_name = alert_settings.get("alert_name")
                         if alert_name == None:
-                                raise Exception(f"No alert name")
+                                raise Exception("No alert name")
                         regis_alert_id = alert_settings.get("regis_id")
                         if regis_alert_id == None:
-                                raise Exception(f"No regis_alert_id")
+                                raise Exception("No regis_alert_id")
                         ignore = alert_settings.get("ignore")
                         if ignore == None:
-                                raise Exception(f"No ignore status")
+                                raise Exception("No ignore status")
                                         
