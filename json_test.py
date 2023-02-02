88838e356fb6575349f16f470e97c76af678d66e
Mikhail <mikhail@MacBook-Pro-Mikhail.local>
2023-02-02 22:06:01 +0300

    ci

--- json_test.py
+++ json_test.py
@@ -3,7 +3,10 @@ import io
 
 
 with io.open("test.json", 'r') as f:
-        settings = json.load(f)
+        try:
+                settings = json.load(f)
+        except Exception as e:
+                print(e)
         alerts_list = settings["alerts"]
         for alert_settings in alerts_list:
                 alert_name = alert_settings.get("alert_name")
