ebe276f83426a6283218db7fed4286758d014ca7
Mikhail <mikhail@192.168.1.11>
2023-02-02 22:39:25 +0300

    ci

--- json_test.py
+++ json_test.py
@@ -5,22 +5,17 @@ import io
 if __name__ == "__main__":
         with io.open("test.json", 'r') as f:
                 settings = json.load(f)
-                alerts_list = settings["alerts"]
-                i = 1
+                alerts_list = settings.get["alerts"]
+                if alerts_list == None:
+                        raise Exception(f"Can not parse json. No alerts list")
                 for alert_settings in alerts_list:
-                        i += 1
-                        try:
-                                alert_name = alert_settings["alert_name"]
-                        except:
-                                print(f"No alert name in string {i}")
-                                raise Exception()
-                        try:
-                                regis_alert_id = alert_settings["regis_id"]
-                        except:
-                                print(f"No regis id in string {i}")
-                                raise Exception()
-                        try:
-                                ignore = alert_settings["ignore"]
-                        except:
-                                print(f"No ignore status in string {i}")
-                                raise Exception()
+                        alert_name = alert_settings.get("alert_name")
+                        if alert_name == None:
+                                raise Exception(f"No alert name")
+                        regis_alert_id = alert_settings.get("regis_id")
+                        if regis_alert_id == None:
+                                raise Exception(f"No regis_alert_id")
+                        ignore = alert_settings.get("ignore")
+                        if ignore == None:
+                                raise Exception(f"No ignore status")
+                                        
