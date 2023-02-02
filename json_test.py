23aa85454fa0f5e73a385dbadc6fa66ac7c9ae55
Mikhail <mikhail@MacBook-Pro-Mikhail.local>
2023-02-02 22:28:34 +0300

    ci

--- json_test.py
+++ json_test.py
@@ -9,16 +9,15 @@ if __name__ == "__main__":
                 i = 1
                 for alert_settings in alerts_list:
                         i += 1
-                        alert_name = alert_settings.get("alert_name")
-                        if alert_name == None:
-                                error = f"No alert name in string {i}"
-                                raise Exception(error)
-                        regis_alert_id = alert_settings.get("regis_id")
-                        if regis_alert_id == None:
-                                error = f"No alert name in string {i}"
-                                raise Exception(error)
-                        ignore = alert_settings.get("ignore")
-                        if ignore == None:
-                                error = f"No ignore status in string {i}"
-                                raise Exception(error)
-                                        
+                        try:
+                                alert_name = alert_settings["alert_name"]
+                        except:
+                                print(f"No alert name in string {i}")
+                        try:
+                                regis_alert_id = alert_settings["regis_id"]
+                        except:
+                                print(f"No regis id in string {i}")
+                        try:
+                                ignore = alert_settings["ignore"]
+                        except:
+                                print(f"No ignore status in string {i}")
